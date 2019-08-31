f318564d3e988589f1ade213563b338d38447eb2
Paulo Vianna Mu <paulomu@live.com>
2019-08-31 14:58:23 -0300

    Commit inicial: Projeto Maven criado a partir do Archetype org.apache.maven.archetypes.maven-archetype-quickstart:RELEASE, ajustado para o Java 8 e JUnit 5.

---
+++ src/main/java/br/pucrs/politecnica/_4636h/marsrover/App.java
@@ -0,0 +1,13 @@
+package br.pucrs.politecnica._4636h.marsrover;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/br/pucrs/politecnica/_4636h/marsrover/AppTest.java
@@ -0,0 +1,20 @@
+package br.pucrs.politecnica._4636h.marsrover;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
