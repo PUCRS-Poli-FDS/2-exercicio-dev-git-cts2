c48fbfbc58a21217f3c6eb34a8d4f88dbdf494a6
dantefleschpereira <48799984+dantefleschpereira@users.noreply.github.com>
2019-09-02 15:23:15 -0300

    Projeto alterado com testes

--- src/test/java/br/pucrs/politecnica/_4636h/marsrover/RoverTest.java
+++ src/test/java/br/pucrs/politecnica/_4636h/marsrover/RoverTest.java
@@ -1,6 +1,7 @@
 package br.pucrs.politecnica._4636h.marsrover;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -30,16 +31,26 @@ public class RoverTest {
 		// Assert
 		assertEquals("0 0 W", rover.getPositionAsString());
 	}
-	
+
 	@Test
 	public void spinRightTest() {
-		
-		//Arrange
-		rover = new Rover(0,0, 'N');
+
+		// Arrange
+		rover = new Rover(0, 0, 'N');
 		// Act
 		rover.runInstructions("R");
 		// Assert
 		assertEquals("0 0 E", rover.getPositionAsString());
+	}
+
+	@Test
+	public void roverDirecaoInvalidaDeveLancarExcecao() {
+
+		// Arrange
+		rover = new Rover(0, 0, 'N');
+		String instructions = "0, 1, 'K'";
+		// Act
+		assertThrows(IllegalArgumentException.class, () -> rover.runInstructions(instructions));
 	}
 
 }
